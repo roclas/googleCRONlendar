18c5f37e40acffa56c13d04eda3ea62ea772c057
carlos <carlos@work.(none)>
2013-08-05 16:02:17 +0200

    minor changes

--- src/main/java/com/roclas/googleCRONlendar_client/App.java
+++ src/main/java/com/roclas/googleCRONlendar_client/App.java
@@ -1,6 +1,9 @@
 package com.roclas.googleCRONlendar_client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
 import com.google.gdata.data.DateTime;
 import com.google.gdata.data.calendar.CalendarEventEntry;
 import com.google.gdata.util.ServiceException;
@@ -11,29 +14,53 @@ import com.google.gdata.util.ServiceException;
  */
 public class App 
 {
-    public static void main( String[] args ) throws IOException, ServiceException
+    private static final String EXECUTE = "execute";
+    
+    public static String command(String cmd){ 
+     String result = "";
+     try 
+     { 
+    	Process p=Runtime.getRuntime().exec(cmd); 
+    	p.waitFor(); 
+    	BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream())); 
+    	String line=reader.readLine(); 
+    	while(line!=null) 
+    	{ 
+    	System.out.println(line); 
+    	result+=line;
+    	line=reader.readLine(); 
+    	} 
+	
+     } 
+     catch(IOException e1) {} 
+     catch(InterruptedException e2) {}
+	return result; 
+    } 
+
+	public static void main( String[] args ) throws IOException, ServiceException
     {
+    	String user=null,password=null;
     	if(args.length<2){
     		usage();
     		return;
     	}
-    	String user=null,password=null;
     	for(int i=0;i<args.length;i++){
-    		switch(i){
-    			case 0:user=args[i];break;
-    			case 1:password=args[i];break;
-    			default:break;
-    		}
+    	    			switch(i){
+    	    			case 0:user=args[i];break;
+    			  			case 1:password=args[i];break;
+    			  			default:break;
+    		  			}
     	}
         CalendarOperator operator = new CalendarOperator(user,password);
         CalendarEventEntry event = operator.getCurrentEvent();
         if(event!=null){
-        	if(event.getTitle().toString().trim().toLowerCase().startsWith("execute")){
-        		System.out.println(event.getTitle()+"-execute-"+event.getPlainTextContent());
-        	        		event.getTimes().get(0).setEndTime((new DateTime()).now());
-        	        		event.update();
-        	}
-        }
+        	//if(event.getTitle().toString().trim().toLowerCase().startsWith("execute")){
+        	if(event.getPlainTextContent().trim().toLowerCase().startsWith(EXECUTE)){
+        		System.out.println( command(event.getPlainTextContent().trim().split(EXECUTE)[1]));
+        	    event.getTimes().get(0).setEndTime((new DateTime()).now());
+        	    event.update();
+        	}else{ System.out.println("No event to run:"+event.getPlainTextContent()+event.getEtag()); }
+        }else{ System.out.println("no events to run"); }
     }
 
 	private static void usage() {
